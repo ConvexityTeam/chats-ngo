ef94d9d54e17d1e58e567af8979dfd9ad8c69803
emmaodia <odia.emma@gmail.com>
2020-09-27 19:09:18 +0100

    Update: Styled Login Page and set Background Image url

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,12 +6,15 @@ import ImageDark from '../assets/img/login-office-dark.jpeg'
 import { GithubIcon, TwitterIcon } from '../icons'
 import { Label, Input, Button } from '@windmill/react-ui'
 
+const car = 'https://images.unsplash.com/flagged/photo-1569118311341-145748f3f338'
+
 function Login() {
   return (
-    <section className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0 src={require('https://images.unsplash.com/flagged/photo-1569118311341-145748f3f338')}" >
+    <section className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style={{ backgroundImage:`url(${car})`, backgroundSize: "cover",
+    height: "100vh", }}>
             <header className="max-w-lg mx-auto">
                 <Link to="#">
-                    <h1 className="text-4xl font-bold text-white text-center">Chats NGO</h1>
+                    <h1 className="flex text-2xl font-bold font-family-Gilroy-Bold text-white text-center justify-center">Welcome to CHATS <p className="text-xs">for NGOs</p></h1>
                 </Link>
             </header>
 
@@ -40,23 +43,29 @@ function Login() {
                           <input className="shadow appearance-none border border-grey-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="" />
                           <p className="text-red-500 text-xs italic"></p>
                         </div>
-                        <div className="flex justify-end">
+                        {/* <div className="flex justify-end">
                             <Link to="#" className="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Forgot your password?</Link>
-                        </div>
-                        <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Sign In</button>
+                        </div> */}
+                        <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Login</button>
                     </form>
+                    <div className="max-w-lg mx-auto text-center mt-12 mb-6">
+                        <p className="text-black">Don't have an account? <Link to="/create-account" className="font-bold hover:text-purple-700 hover:underline">Sign Up</Link>.</p>
+                    </div>
+                    <div className="flex justify-center">
+                        <Link to="#" className="text-sm text-black-600 hover:text-purple-700 hover:underline mb-6">Forgot your password?</Link>
+                    </div>
                 </section>
             </main>
 
-            <div className="max-w-lg mx-auto text-center mt-12 mb-6">
-                <p className="text-white">Have an account? <Link to="#" className="font-bold hover:underline">Register</Link>.</p>
-            </div>
+            {/* <div className="max-w-lg mx-auto text-center mt-12 mb-6">
+                <p className="text-white">Don't have an account? <Link to="#" className="font-bold hover:underline">Sign Up</Link>.</p>
+            </div> */}
 
-            <footer className="max-w-lg mx-auto flex justify-center text-white">
+            {/* <footer className="max-w-lg mx-auto flex justify-center text-white">
                 <Link to="#" className="hover:underline">Contact</Link>
                 <span className="mx-3">•</span>
                 <Link to="#" className="hover:underline">Privacy</Link>
-            </footer>
+            </footer> */}
             </section>
 
     // <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
