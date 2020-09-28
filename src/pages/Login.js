a7041c5bc49b0661780ba958524bdc215dcc346d
emmaodia <odia.emma@gmail.com>
2020-09-28 15:19:49 +0100

    CHORE: Implemented Login to Backend.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,92 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { isEmail } from "validator"
+
+import AuthService from "../services/auth.service";
 // import '../styles/app.css'
 import ImageLight from '../assets/img/login-office.jpeg'
 import ImageDark from '../assets/img/login-office-dark.jpeg'
 import { GithubIcon, TwitterIcon } from '../icons'
-import { Label, Input, Button } from '@windmill/react-ui'
+// import { Label, Input, Button } from '@windmill/react-ui'
 
-const car = 'https://images.unsplash.com/flagged/photo-1569118311341-145748f3f338'
+const backgroundImage = 'https://images.unsplash.com/flagged/photo-1569118311341-145748f3f338'
 
-function Login() {
+const required = value => {
+  if (!value) {
+    return (
+      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <span class="block sm:inline">This field is required!</span>
+        </div>
+    );
+  }
+};
+
+const validEmail = value => {
+  if (!isEmail(value)) {
+    return (
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <span class="block sm:inline">Please enter valid email.</span>
+        </div>
+    );
+  }
+};
+
+const Login = (props) => {
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    setMessage("");
+    setLoading(true);
+
+    form.current.validateAll();
+
+    if (checkBtn.current.context._errors.length === 0) {
+      AuthService.login(email, password).then(
+        () => {
+          props.history.push("/app/dashboard");
+          window.location.reload();
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          setLoading(false);
+          setMessage(resMessage);
+        }
+      );
+    } else {
+      setLoading(false);
+    }
+  };
+
   return (
-    <section className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style={{ backgroundImage:`url(${car})`, backgroundSize: "cover",
+    <section className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style={{ backgroundImage:`url(${backgroundImage})`, backgroundSize: "cover",
     height: "100vh", }}>
             <header className="max-w-lg mx-auto">
                 <Link to="#">
@@ -25,7 +101,7 @@ function Login() {
                 </section> */}
 
                 <section className="mt-10">
-                    <form className="flex flex-col" method="POST" action="#">
+                    <Form className="flex flex-col" onSubmit={handleLogin} ref={form}>
                         {/* <div className="mb-6 pt-3 rounded bg-gray-200">
                             <label className="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Name of Organization</label>
                             <input type="text" id="email" className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" />
@@ -34,20 +110,61 @@ function Login() {
                           <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
                             Email
                           </label>
-                          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="" />
+                          <Input 
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" 
+                            name="email" 
+                            onChange={onChangeEmail}
+                            type="text" 
+                            validations={[required, validEmail]} 
+                            placeholder="" 
+                          />
                         </div>
                         <div className="mb-6">
                           <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
                             Password
                           </label>
-                          <input className="shadow appearance-none border border-grey-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="" />
+                          <Input 
+                            className="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" 
+                            name="password" 
+                            onChange={onChangePassword}
+                            type="password" 
+                            validations={[required]} 
+                            placeholder="" 
+                          />
                           <p className="text-red-500 text-xs italic"></p>
                         </div>
                         {/* <div className="flex justify-end">
                             <Link to="#" className="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Forgot your password?</Link>
                         </div> */}
-                        <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Login</button>
-                    </form>
+                        {/* <Link to='/app/dashboard' className="bg-purple-600 hover:bg-purple-700 text-white text-center font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" 
+                        // type="submit"
+                        >Login
+                        </Link> */}
+                        {/* <div className="form-group"> */}
+                        <button className="bg-purple-600 hover:bg-purple-700 text-white text-center font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" disabled={loading}>
+                            {loading && (
+                              <span className="spinner-border spinner-border-sm"></span>
+                            )}
+                            <span >Login</span>
+                          </button>
+                        {/* </div> */}
+
+                        {message && (
+                          <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                          {/* <strong class="font-bold">Holy smokes!</strong> */}
+                           <span class="block sm:inline">{message}</span>
+                          </div>
+                          // <div className="form-group">
+                          //   <div className="alert alert-danger" role="alert">
+                          //     {message}
+                          //   </div>
+                          // </div>
+                        )}
+                        <CheckButton
+                          style={{ display: "none" }}
+                          ref={checkBtn}
+                        />
+                    </Form>
                     <div className="max-w-lg mx-auto text-center mt-12 mb-6">
                         <p className="text-black">Don't have an account? <Link to="/create-account" className="font-bold hover:text-purple-700 hover:underline">Sign Up</Link>.</p>
                     </div>
