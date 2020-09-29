e3a87ce76ed3e66d41db6efe02f343858b897465
emmaodia <odia.emma@gmail.com>
2020-09-29 17:18:36 +0100

    UPDATE: Disabled direct routing via url path.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { lazy } from 'react'
+import React, { lazy, useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer'
+
+import AuthService from "./services/auth.service";
 
 const Layout = lazy(() => import('./containers/Layout'))
 const Login = lazy(() => import('./pages/Login'))
@@ -8,19 +10,37 @@ const CreateAccount = lazy(() => import('./pages/CreateAccount'))
 const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
 
 function App() {
+  const [currentNGO, setCurrentNGO] = useState(undefined);
+
+  useEffect(() => {
+    const organization = AuthService.getCurrentNGO();
+
+    if (organization) {
+      setCurrentNGO(organization);
+    }
+  }, []);
+
   return (
     <>
       <Router>
         <AccessibleNavigationAnnouncer />
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/create-account" component={CreateAccount} />
-          <Route path="/forgot-password" component={ForgotPassword} />
+          {currentNGO ? (
+            <Route path="/app" component={Layout} />
+          ) : (
+            <>
+              <Route path="/login" component={Login} />
+              <Route path="/create-account" component={CreateAccount} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+              <Redirect exact from="/" to="/login" />
+            </>
+          )}
+          
 
           {/* Place new routes over this */}
-          <Route path="/app" component={Layout} />
+          
           {/* If you have an index page, you can remothis Redirect */}
-          <Redirect exact from="/" to="/login" />
+          
         </Switch>
       </Router>
     </>
