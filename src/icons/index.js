252b1df39c160aea1046ea6aac756e7bf2fe3c81
emmaodia <odia.emma@gmail.com>
2020-09-29 16:10:59 +0100

    LOADED NEW ICONS.

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,6 @@
 import { ReactComponent as ButtonsIcon } from './buttons.svg'
 import { ReactComponent as CardsIcon } from './cards.svg'
+import { ReactComponent as CreditCardIcon } from './credit-card.svg'
 import { ReactComponent as ChartsIcon } from './charts.svg'
 import { ReactComponent as FormsIcon } from './forms.svg'
 import { ReactComponent as HomeIcon } from './home.svg'
@@ -12,25 +13,33 @@ import { ReactComponent as TrashIcon } from './trash.svg'
 import { ReactComponent as ForbiddenIcon } from './forbidden.svg'
 import { ReactComponent as GithubIcon } from './github.svg'
 import { ReactComponent as TwitterIcon } from './twitter.svg'
+import { ReactComponent as LogOutIcon } from './logout.svg'
 import { ReactComponent as MailIcon } from './mail.svg'
 import { ReactComponent as CartIcon } from './cart.svg'
 import { ReactComponent as ChatIcon } from './chat.svg'
+import { ReactComponent as MegaphoneIcon } from './megaphone.svg'
 import { ReactComponent as MoneyIcon } from './money.svg'
 import { ReactComponent as PeopleIcon } from './people.svg'
 import { ReactComponent as SearchIcon } from './search.svg'
+import { ReactComponent as SettingsIcon } from './settings.svg'
+import { ReactComponent as ShoppingIcon } from './shopping-bag.svg'
 import { ReactComponent as MoonIcon } from './moon.svg'
 import { ReactComponent as SunIcon } from './sun.svg'
+import { ReactComponent as SupportHeadphonesIcon } from './support-headphones.svg'
 import { ReactComponent as BellIcon } from './bell.svg'
 import { ReactComponent as MenuIcon } from './menu.svg'
 import { ReactComponent as DropdownIcon } from './dropdown.svg'
 import { ReactComponent as OutlinePersonIcon } from './outlinePerson.svg'
 import { ReactComponent as OutlineCogIcon } from './outlineCog.svg'
 import { ReactComponent as OutlineLogoutIcon } from './outlineLogout.svg'
+import { ReactComponent as UsersIcon } from './users.svg'
+import { ReactComponent as VendorBoothIcon } from './vendor-booth.svg'
 
 export {
   ButtonsIcon,
   CardsIcon,
   ChartsIcon,
+  CreditCardIcon,
   FormsIcon,
   HomeIcon,
   ModalsIcon,
@@ -45,15 +54,22 @@ export {
   MailIcon,
   CartIcon,
   ChatIcon,
+  LogOutIcon,
+  MegaphoneIcon,
   MoneyIcon,
   PeopleIcon,
   SearchIcon,
   MoonIcon,
   SunIcon,
+  SupportHeadphonesIcon,
+  ShoppingIcon,
+  SettingsIcon,
   BellIcon,
   MenuIcon,
   DropdownIcon,
   OutlinePersonIcon,
   OutlineCogIcon,
   OutlineLogoutIcon,
+  UsersIcon,
+  VendorBoothIcon
 }
