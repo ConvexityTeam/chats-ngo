7099371dcbf751c277766f8348c9dc7f267da65b
emmaodia <odia.emma@gmail.com>
2020-09-30 02:16:35 +0100

    WIP: Organization Campaign Calls.

--- src/services/organization.service.js
+++ src/services/organization.service.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 import authHeader from "./auth-header";
+const organization = JSON.parse(localStorage.getItem('organization'));
+
+const id = organization.data
+
+console.log(id)
+
 
 const GET_ALL_NGO_URL = "https://chats-backend.herokuapp.com/api/v1/ngos/";
 const GET_ONE_NGO_URL = "https://chats-backend.herokuapp.com/api/v1/ngos/";
@@ -9,7 +15,7 @@ const getAllNGO = () => {
 };
 
 const getOneNGO = () => {
-  return axios.get(GET_ONE_NGO_URL, { headers: authHeader() });
+  return axios.get(`${GET_ONE_NGO_URL}${id}`, { headers: authHeader() });
 };
 
 export default {
