73fd4bedf29680de9302ecbf577e263c562c5a6e
emmaodia <odia.emma@gmail.com>
2020-11-29 03:50:53 +0100

    CHORE: Legend title and color set for Beneficiaries Age.

--- src/components/Chart/ChartLegend.js
+++ src/components/Chart/ChartLegend.js
@@ -5,8 +5,8 @@ function ChartLegend({ legends }) {
     <div className="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
       {legends.map((legend) => (
         <div className="flex items-center" key={legend.title}>
-          {/* <span className={`inline-block w-3 h-3 mr-1 ${legend.color} rounded-full`}></span> */}
-          {/* <span>{legend.title}</span> */}
+          <span className={`inline-block w-3 h-3 mr-1 ${legend.color} rounded-full`}></span>
+          <span>{legend.title}</span>
         </div>
       ))}
     </div>
