59d7d194051cf1015b3844d21a526efea4060637
Prince <chimeremezechukwudire@gmail.com>
2020-12-11 09:30:48 +0100

    folder structure added

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="main">
+wwjw
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+.main{
+  background-image:url('../assets/img/Cash For Work.png') ;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh
+}
+</style>
