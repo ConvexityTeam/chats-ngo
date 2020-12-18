e381301ea67bd74911a212ed1d04fb85cd9db46f
Prince <chimeremezechukwudire@gmail.com>
2020-12-18 09:41:01 +0100

    feature: all vendors table added

--- components/tables/vendor-transaction.vue
+++ components/tables/vendor-transaction.vue
@@ -1,25 +1,115 @@
 <template>
-    <div>
-     <div class="d-flex pt-lg-4">
-        <div>
-          <!-- Search Box here -->
-          <input
-            type="text"
-            class="form-controls"
-            placeholder="Search transactions"
-          />
-        </div>
+  <div>
+    <div class="d-flex pt-lg-4">
+      <div>
+        <!-- Search Box here -->
+        <input
+          type="text"
+          class="form-controls"
+          placeholder="Search transactions"
+        />
+      </div>
 
-        <div class="ml-auto">
-     <button type="button" class="export-btn p-3">Export as CSV</button>
-        </div>
+      <div class="ml-auto">
+        <button type="button" class="export-btn p-3">Export as CSV</button>
       </div>
     </div>
+
+    <!-- Transactions Table here -->
+    <div class="holder">
+      <div class="d-flex px-4 pt-3">
+        <div>
+          <h4 class="header">Trasactions</h4>
+        </div>
+
+        <div class="ml-auto d-flex filter">
+          <p>Filter by:</p>
+          <div class="mx-2">
+            <select name="" id="">
+              <option value="gender">Today</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <table class="table table-borderless">
+        <thead>
+          <tr>
+            <th scope="col">Reference ID</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Vendor</th>
+            <th scope="col">Beneficiary</th>
+            <th scope="col">Date</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in 3" :key="i">
+            <td>12345678910</td>
+            <td>$123,476,000</td>
+            <td>Dangote</td>
+            <td>Funke</td>
+            <td>12 Dec, 2020</td>
+            <td>
+              <button type="button" class="more-btn"><dot /></button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+<script>
+import dot from '~/components/icons/dot'
+export default {
+  components: {
+    dot
+  },
+}
+</script>
 
 <style scoped>
-    .form-controls {
+.form-controls {
   height: 50px;
   width: 150%;
+}
+.header {
+  color: var(--secondary-black);
+  font-weight: 700;
+  font-size: 1.125rem;
+}
+.filter {
+  color: #4f4f4f;
+  font-size: 1rem;
+  font-weight: 500;
+}
+select {
+  border: none;
+  color: var(--secondary-black);
+}
+select:focus {
+  outline: none;
+}
+.holder {
+  background: #ffffff;
+  box-shadow: 0px 4px 30px rgba(174, 174, 192, 0.2);
+  border-radius: 10px;
+  margin-top: 30px;
+}
+
+.table thead th {
+  color: #4f4f4f;
+  letter-spacing: 0.01em;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.table th,
+.table td {
+  padding: 0.75rem 2rem;
+  color: var(--secondary-black);
+  font-size: 1rem;
+}
+::placeholder{
+    color: #999999;
+font-size: 1rem;
 }
 </style>
--- pages/vendors/all-vendors.vue
+++ pages/vendors/all-vendors.vue
@@ -1,11 +1,91 @@
 <template>
-    <div>
-    ALL VENDORS HERE
+  <div>
+    <h4 class="header">All Vendors</h4>
+
+     <div class="d-flex pt-lg-4 mr-4">
+      <div>
+        <!-- Search Box here -->
+        <input
+          type="text"
+          class="form-controls"
+          placeholder="Search vendors by name or ID"
+        />
+      </div>
+
+      <div class="ml-auto">
+        <button type="button" class="export-btn p-3">Export as CSV</button>
+      </div>
     </div>
+
+     <!-- Vendors Table here -->
+    <div class="holder">
+      <table class="table table-borderless">
+        <thead>
+          <tr>
+           <th scope="col"><input type="checkbox"></th>
+            <th scope="col">Selected 2 vendors</th>
+            <th scope="col">Vendor ID</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Email Address</th>
+            <th scope="col">Account Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in 3" :key="i">
+             <td><input type="checkbox"></td>
+            <td class="d-flex"> <img src="~/assets/img/user.png" width="30" height="30" class="rounded" alt=""> <p class="mx-3 pt-1">Cadbury Ng</p> </td>
+            <td>DEV63016762</td>
+            <td>2348013123065</td>
+            <td>rsnyder@blogspan.gov</td>
+            <td>04/28/2018</td>
+   
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    layout:"dashboard"
+  layout: 'dashboard',
 }
 </script>
+
+<style scoped>
+.header {
+  color: var(--secondary-black);
+  font-weight: 700;
+  line-height: 28px;
+  letter-spacing: 0.01em;
+  font-size: 1.5rem;
+}
+.form-controls {
+  height: 50px;
+  width: 150%;
+}
+::placeholder{
+    color: #999999;
+font-size: 1rem;
+}
+.holder {
+  background: #ffffff;
+  box-shadow: 0px 4px 30px rgba(174, 174, 192, 0.2);
+  border-radius: 10px;
+  margin-top: 30px;
+}
+.table thead th {
+  color: #4f4f4f;
+  letter-spacing: 0.01em;
+  font-size: 1rem;
+  font-weight: 700;
+  line-height: 20px;
+letter-spacing: -0.05px;
+}
+.table th,
+.table td {
+  padding: 0.75rem 2rem;
+color: #3A3B3F;
+  font-size: 0.875rem;
+}
+</style>
